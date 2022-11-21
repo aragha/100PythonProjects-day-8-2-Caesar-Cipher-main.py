3d810d94d1a056a26907e3286e6665f4409af99e
resplit <67502036+aragha@users.noreply.github.com>
2022-11-21 22:21:24 +0000

    updated the order of a few print statements to make it more meaningful

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from art import logo
 alphaforward = 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz'
 alphareverse = 'zyxwvutsrqponmlkjihgfedcbazyxwvutsrqponmlkjihgfedcbazyxwvutsrqponmlkjihgfedcba'
 def encrypt(alphabets, encdec, inputtext, shiftrequired):
@@ -11,6 +12,7 @@ def encrypt(alphabets, encdec, inputtext, shiftrequired):
 
 alphabet = list(alphaforward)
 alphabetreversed = list(alphareverse)
+print(logo)
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
@@ -32,6 +34,7 @@ shift = int(input("Type the shift number:\n"))
 #TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
 if direction == 'encode':
   result = encrypt(alphabet, direction, text, shift)
+  print(f"The encrypted word is {result}")  
 else:
   result = encrypt(alphabetreversed, direction, text, shift)
-print(result)
+  print(f"The decrypted word is {result}")
